4b52d447d92f7fdaa1e91e8dd42ed6f2f6c5bf68
Alexey Nurgaliev <atnurgaliev@gmail.com>
2015-10-10 15:04:34 +0300

    advanced command syntax

--- src/main/java/ru/doublebyte/telegramWeatherBot/utils/Command.java
+++ src/main/java/ru/doublebyte/telegramWeatherBot/utils/Command.java
@@ -1,10 +1,12 @@
 package ru.doublebyte.telegramWeatherBot.utils;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Bot command
- * Trivial implementation
+ * Starts with /
+ * Arguments are separated by space
  */
 public class Command {
 
@@ -15,11 +17,74 @@ public class Command {
         if(command == null || command.length() < 2 || !command.startsWith("/")) {
             throw new IllegalArgumentException("Not a command");
         }
+        this.command = command.split(" ")[0].substring(1);
 
-        String[] parts = command.split(" ");
+        String argumentsStr = command.substring(this.command.length() + 1).trim();
 
-        this.command = parts[0].substring(1);
-        this.args = Arrays.copyOfRange(parts, 1, parts.length);
+        List<String> arguments = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        boolean insideOfQuotes = false;
+
+        for(int charIdx = 0; charIdx < argumentsStr.length(); charIdx++) {
+            char character = argumentsStr.charAt(charIdx);
+
+            switch(character) {
+                //quoted string ignores whitespace
+                case '"':
+                    if(insideOfQuotes) {
+                        if(sb.length() != 0) {
+                            arguments.add(sb.toString());
+                            sb = new StringBuilder();
+                        }
+                        insideOfQuotes = false;
+                    } else {
+                        insideOfQuotes = true;
+                    }
+                    break;
+
+                //whitespace is argument delimiter (unless in quoted string)
+                case ' ':
+                    if(!insideOfQuotes) {
+                        if(sb.length() != 0) {
+                            arguments.add(sb.toString());
+                            sb = new StringBuilder();
+                        }
+                    } else {
+                        sb.append(character);
+                    }
+                    break;
+
+                //escape character
+                case '\\':
+                    if(charIdx == argumentsStr.length() - 1) {
+                        break;
+                    }
+                    char nextCharacter = argumentsStr.charAt(charIdx+1);
+                    charIdx++;
+
+                    switch(nextCharacter) {
+                        case '"':
+                            sb.append('"');
+                            break;
+                        case 'n':
+                            sb.append('\n');
+                            break;
+                        case 't':
+                            sb.append('\t');
+                            break;
+                    }
+                    break;
+
+                default:
+                    sb.append(character);
+            }
+        }
+
+        if(sb.length() > 0) {
+            arguments.add(sb.toString());
+        }
+
+        this.args = arguments.toArray(new String[arguments.size()]);
     }
 
     ///////////////////////////////////////////////////////////////////////////
--- src/test/java/ru/doublebyte/telegramWeatherBot/utils/CommandTest.java
+++ src/test/java/ru/doublebyte/telegramWeatherBot/utils/CommandTest.java
@@ -11,5 +11,10 @@ public class CommandTest {
         Command command = new Command("/command arg0 arg1");
         assertEquals("command", command.getCommand());
         assertArrayEquals(new String[]{"arg0", "arg1"}, command.getArgs());
+
+        command = new Command("/command \"arg0 arg1\" arg\\n2 arg\\t3 a\"rg4 arg5\" a\\\"rg6 arg7");
+        assertEquals("command", command.getCommand());
+        assertArrayEquals(new String[]{"arg0 arg1", "arg\n2", "arg\t3", "arg4 arg5", "a\"rg6", "arg7"},
+                command.getArgs());
     }
 }
